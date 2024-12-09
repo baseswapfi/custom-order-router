d9212dd62595324ac170c1669637bba5cd096a9f
0xshoegazer <0xshoegazer@gmail.com>
2024-12-09 07:22:02 -0500

    util function

--- src/util/index.ts
+++ src/util/index.ts
@@ -1,3 +1,5 @@
+import { Token } from '@baseswapfi/sdk-core';
+
 export * from './addresses';
 export * from './amounts';
 export * from './chains';
@@ -6,3 +8,20 @@ export * from './log';
 export * from './metric';
 export * from './protocols';
 export * from './routes';
+
+/**
+ * Returns true if the address of this token sorts before the address of the other token
+ * @param other other token to compare
+ * @throws if the tokens have the same address
+ * @throws if the tokens are on different chains
+ */
+export function sortsBefore(currencyA: Token, currencyB: Token): boolean {
+  // invariant(currencyA.chainId === currencyB.chainId, 'CHAIN_IDS');
+  // invariant(this.address.toLowerCase() !== other.address.toLowerCase(), 'ADDRESSES');
+
+  if (currencyA.chainId !== currencyB.chainId) throw new Error('CHAIN_IDS');
+  if (currencyA.address.toLowerCase() === currencyB.address.toLowerCase())
+    throw new Error('ADDRESSES');
+
+  return currencyA.address.toLowerCase() < currencyB.address.toLowerCase();
+}
